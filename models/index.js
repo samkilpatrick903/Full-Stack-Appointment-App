0e9d3ebbb608f79bb298f55563c42d551734c45e
Chuck Stephens <chuckstephens2076@gmail.com>
2021-11-01 18:03:11 -0500

    lined up models and seeds to match data fields

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const Patient = require('./Patient');
 const Appointments = require('./Appointments');
-
+const Doctor = require('./Doctor');
 
 Patient.hasMany(Appointments, {
   foreignKey: 'Patient_id',
@@ -11,4 +11,4 @@ Appointments.belongsTo(Patient, {
   foreignKey: 'Patient_id'
 });
 
-module.exports = { Patient, Appointments };
+module.exports = { Patient, Appointments, Doctor };
