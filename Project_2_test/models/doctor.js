7ee3e80e6c4decd42f8d35b4b36d6bc8546e44aa
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-01 11:16:54 -0500

    updating models

--- Project_2_test/models/doctor.js
+++ Project_2_test/models/doctor.js
@@ -2,3 +2,35 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+class doctor extends Model {}
+
+doctor.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      starting_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      ending_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'gallery',
+    }
+  );
+  
+  module.exports = Gallery;
