67546be4dec0972637c23e3f85860c6d68e065ff
Ismeny Saguilan <menysag2@gmail.com>
2021-11-01 14:15:55 -0500

    updated folder structure to correctly import and export

--- models/doctor.js
+++ models/doctor.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class doctor extends Model {}
+class Doctor extends Model {}
 
 doctor.init(
   {
