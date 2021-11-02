0ef8b8711009523bac7b80f92e064d46ffced815
Ismeny Saguilan <menysag2@gmail.com>
2021-11-02 09:15:45 -0500

    added get,put,delete routes to patient/ doctor/ and appointments

--- controllers/api/doctorRoutes.js
+++ controllers/api/doctorRoutes.js
@@ -34,4 +34,54 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+
+router.get('/', async (req, res) => {
+  // find all doctor
+  try {
+    const doctorData = await Doctor.findAll({
+      // be sure to include its associated Products
+      include: [{ model: Patient }],
+    });
+    res.status(200).json(doctorData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one doctor by its `id` value
+  try {
+    const doctorData = await Doctor.findByPk(req.params.id,{
+  // be sure to include its associated Products
+  include: [{ model: Patient }],
+    });
+    if (doctorData)
+    res.status(200).json(doctorData);
+    else {
+      res.status(400).json("Does not exist!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  });
+
+  router.put('/:id', async (req, res) => {
+    // update a doctor by its `id` value
+    try {
+      const doctorData = await Doctor.update(req.body,{
+       where: {
+         id: req.params.id,
+       },
+      });
+      if (!doctorData) {
+        res.status(404).json({ message: 'No doctor with this id!' });
+        return;
+      }
+      res.status(200).json(doctorData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
 module.exports = router;
