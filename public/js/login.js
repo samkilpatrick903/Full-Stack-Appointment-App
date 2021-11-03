4b3e4bffa85be36dc9fc70dd5268cef9d5bf46c2
Ismeny Saguilan <menysag2@gmail.com>
2021-11-03 18:43:05 -0500

    fixed buttons on login and sign up

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/api/patient/login', {
+    const response = await fetch('/api/patient', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/');
+      document.location.replace('/appointments');
     } else {
       alert('Failed to log in');
     }
@@ -37,7 +37,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/homepage');
+      document.location.replace('/appointments');
     } else {
       alert(response.statusText);
     }
