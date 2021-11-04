bd04e4123214af9b99916dc463052d60d778b3da
Carlos Hernandez <90423176+Mrher13@users.noreply.github.com>
2021-11-04 10:24:07 -0500

    adding file to index

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:public/js/login.js
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
@@ -18,10 +19,33 @@ const loginFormHandler = async (event) => {
       document.location.replace('/appointments');
     } else {
       alert(response.statusText);
+=======
+const loginFormHandler = async(event) => {
+    event.preventDefault();
+
+    // Collect values from the login form
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        // Send a POST request to the API endpoint
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // If successful, redirect the browser to the profile page
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+>>>>>>> Stashed changes:Project_2_test/public/js/login.js
     }
-  }
 };
 
+<<<<<<< Updated upstream:public/js/login.js
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
@@ -40,14 +64,43 @@ const signupFormHandler = async (event) => {
       document.location.replace('/appointments');
     } else {
       alert(response.statusText);
+=======
+const signupFormHandler = async(event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (name && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+>>>>>>> Stashed changes:Project_2_test/public/js/login.js
     }
-  }
 };
 
 document
+<<<<<<< Updated upstream:public/js/login.js
   .querySelector('.login-form-1')
   .addEventListener('submit', loginFormHandler);
 
 document
   .querySelector('.login-form-2')
   .addEventListener('submit', signupFormHandler);
+=======
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+>>>>>>> Stashed changes:Project_2_test/public/js/login.js
